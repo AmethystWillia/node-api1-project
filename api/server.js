3e6508b4bb0a094ad433f34c574ea4eca06af0b6
Amethyst Willia <zmithic@gmail.com>
2022-02-07 15:11:31 -0600

    Added get request for all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,24 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The users information could not be retrieved" });
+        })
+});
+
+// server.post();
+
+// server.put();
+
+// server.delete();
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
